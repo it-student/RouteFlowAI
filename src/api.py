a4557b85c27f16693cfbc05542c49df12d3e0592
Yasin Akkus <akkusoft@gmail.com>
2026-07-15 00:21:05 +0200

    Added Create, Read functionality for User-Table

--- src/api.py
+++ src/api.py
@@ -1,19 +1,45 @@
 """
 This module will define the API by defining routes and their allowed methods, etc.
 """
-from pydantic import BaseModel, ConfigDict
+from fastapi import HTTPException
+import db.schemas as schemas
+import models
+from db.db_operations import engine, SessionLocal
+from sqlalchemy.orm import Session
 
-# For data coming IN (POST requests)
-class UserCreate(BaseModel):
-    name: str | None
-    family_name: str
-    address: str
-    gps_coordinates: str | None
 
-# For data going OUT (Responses)
-class UserResponse(BaseModel):
-    id: int
-    name: str | None
-    family_name: str
-    address: str
+def users_fetchall(db: Session):
+    """
+    get all users
+    :rtype: list[type[User]]
+    :param db: Session object for database access
+    :return users: All users existing.
+    """
+    users = db.query(schemas.User).all()
+    return users
+
+def users_get(user_id: int, db: Session):
+    """
+    get a user by its id
+    :param user_id: the unique id of the user
+    :param db: Session object for database access
+    :return:
+    """
+    user = db.query(schemas.User).filter(schemas.User.id == user_id).first()
+    return user
+
+def users_create(user: models.UserCreate, db: Session):
+    """
+    create a new user
+    :param user: UserCreate object
+    :param db: Session object for database access
+    :return user: UserResponse object
+    """
+    new_user = schemas.User(name = user.name,
+                            family_name = user.family_name,
+                            address = user.address,
+                            gps_coordinates = user.gps_coordinates)
+    db.add(new_user)
+    db.commit()
+    return new_user
 
