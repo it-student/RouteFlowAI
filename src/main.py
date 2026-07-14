55bb3f69ca0281de92bba3a05ea22b2df733c4e1
Yasin Akkus <akkusoft@gmail.com>
2026-07-15 01:45:51 +0200

    Implemented whole C.R.U.D. functionality according to users-table.

--- src/main.py
+++ src/main.py
@@ -3,12 +3,14 @@ starting the fastAPI app.
 """
 from typing import Annotated, List
 
-from fastapi import FastAPI, Depends, HTTPException, Path, Query
+from fastapi import FastAPI, Depends, HTTPException, Path, Body
 from db.db_operations import engine, SessionLocal
 from sqlalchemy.orm import Session
 import db.schemas as models
 import models as schemas
-from api import users_fetchall, users_create, users_get
+from api import users_fetchall, users_create, users_get, users_update, users_delete
+
+
 
 def get_db():
     """
@@ -36,8 +38,6 @@ def hello_world():
 def read_all_users(db: db_dependency):
     """
     - Get all users, if no user_id given, user with user_id otherwise
-    - Calls api.read_users(user_id)
-    - :param db: Session-object for database access
     - :return users: list of UserResponse objects:
     """
     users = users_fetchall(db)
@@ -53,10 +53,9 @@ def get_user(db: db_dependency,
                   examples=[40451]  # Populates the path input field
               )):
     """
-    Retrieves a user by its ID.
-    :param db: Session object for database access
-    :param user_id: unique id of the user (int)
-    :return user: UserResponse object:
+    - Retrieves a user by its ID.
+    - :param user_id: unique id of the user (int)
+    - :return user: UserResponse object:
     """
     user = users_get(db=db, user_id=user_id)
     if not user:
@@ -66,11 +65,44 @@ def get_user(db: db_dependency,
 @app.post("/users/", response_model=schemas.UserResponse)
 async def create_user(user: schemas.UserCreate, db: db_dependency):
     """
-    create a new user
-    :param user: UserCreate object
-    :param db: Session object for database access
-    :return user: UserResponse object
+    - create a new user
+    - :param user: UserCreate object
+    - :return user: UserResponse object
     """
     return users_create(user=user, db=db)
 
+@app.put("/users/{user_id}", response_model=schemas.UserResponse)
+async def update_user(db: db_dependency,
+                      user_id: int = Path(
+                          description="The unique ID of the user",
+                          examples=[40451]
+                      ),
+
+                      user: schemas.UserCreate = Body(
+                          description="UserCreate object",
+                          examples=[
+                              {"name": "Max",
+                               "family_name": "Mustermann",
+                               "address": "Musterstr. 12, 12345 Musterstadt",
+                               "gps_coordinates": ""}
+                          ])):
+    """
+    - update a user, finding it by its ID.
+    - :param user_id: unique id of the user (int)
+    - :return user: UserResponse object:
+    """
+    return users_update(db=db, user_id=user_id, user=user)
+
+@app.delete("/users/{user_id}", response_model=schemas.UserResponse)
+async def delete_user(db: db_dependency,
+                      user_id: int = Path(
+                          description="The unique ID of the user",
+                          examples=[40451]  # Populates the path input field
+                      )):
+    """
+    - delete a user by its ID.
+    - :param user_id: unique id of the user (int)
+    - :return user: UserResponse object:
+    """
+    return users_delete(db=db, user_id=user_id)
 
