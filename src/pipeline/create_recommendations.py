25131f2a77bf09b0cf10601cdbf8d054fb81c864
Yasin Akkus <akkusoft@gmail.com>
2026-09-06 23:41:03 +0200

    Removed unnecessary print command

--- src/pipeline/create_recommendations.py
+++ src/pipeline/create_recommendations.py
@@ -1,7 +1,25 @@
 """
 This mocule is for ...
 """
-from ai_functions import get_address, prepare_recommendation_prompt, do_search, structure_output
+from pipeline.ai_functions import prepare_recommendation_prompt, do_search, structure_output
+from db.crud import get_user_address, save_recommendations
+
+#  Adresse des Users abfragen. Wenn keine Adresse gegeben
+#  -> Fallback zu eingetragener Adresse in Users-table, wenn dort immer noch keine Adresse
+#  -> Error: Keine Startadresse gegeben.
+def get_address(search_obj):
+    starting_point = search_obj.starting_point
+    if not starting_point:
+        user_address = get_user_address(search_obj.user_id)
+        if not user_address:
+            raise Exception(f"User with id {search_obj.user_id} does not exist")
+        else:
+            if user_address:
+                starting_point = user_address
+            else:
+                raise Exception(f"User with id {search_obj.user_id} does not contain an address.")
+
+    return starting_point
 
 def recommendations_flow(search_obj):
     """
@@ -17,5 +35,8 @@ def recommendations_flow(search_obj):
     recommendation_prompt = prepare_recommendation_prompt(prompt_params)
     unstructured_results = do_search(recommendation_prompt)
     structured_suggestions = structure_output(unstructured_results)
+    save_recommendations(user_id=prompt_params.user_id,
+                         search_id=prompt_params.id,
+                         suggestion_list=structured_suggestions)
 
     return structured_suggestions
