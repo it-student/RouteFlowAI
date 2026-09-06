1d6297cf5fa59e93d39b3ec81696731342d11d13
Yasin Akkus <akkusoft@gmail.com>
2026-09-06 23:40:07 +0200

    Removed print command from method structure_output()

--- src/pipeline/ai_functions.py
+++ src/pipeline/ai_functions.py
@@ -5,51 +5,12 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import models
-from db import schemas
-from db.db_operations import SessionLocal
 
 load_dotenv()
 
 client = genai.Client()
 
 api_key = os.getenv("GEMINI_API_KEY")
-
-def recommendations_flow(search_obj):
-    """
-    creating a recommendation flow, starting from the given search object.
-    Do a Google search for given search object.
-    receive up to 5 suggestions according to the given search object.
-    structure the search result output for following operations and return them.
-    :param search_obj: A models.SeachCreate object.
-    :return structured_suggestions:
-    """
-    prompt_params = search_obj
-    prompt_params.starting_point = get_address(search_obj=search_obj)
-    recommendation_prompt = prepare_recommendation_prompt(prompt_params)
-    unstructured_results = do_search(recommendation_prompt)
-    structured_suggestions = structure_output(unstructured_results)
-
-    return structured_suggestions
-
-def structure_output(unstructured_results):
-    """
-    Structure the output of unstructured search results.
-    :param unstructured_results: A prior LLM Google-Search result output text.
-    :return suggestions: A models.SuggestionList object.
-    """
-    prompt = "Please extract the relevant information from given input below. \n" + unstructured_results
-    interaction = client.interactions.create(
-        model="gemini-3.1-flash-lite",
-        input=prompt,
-        response_format={
-            "type": "text",
-            "mime_type": "application/json",
-            "schema": models.SuggestionList.model_json_schema()
-        },
-    )
-    print(interaction.output_text, "\n\n")
-    suggestions = models.SuggestionList.model_validate_json(interaction.output_text)
-    return suggestions
 
 def prepare_recommendation_prompt(prompt_params):
 
@@ -79,22 +40,7 @@ together with other given attributes below and present me with up to 5 search re
     "group_size": 1
 """
 
-#  Adresse des Users abfragen. Wenn keine Adresse gegeben
-#  -> Fallback zu eingetragener Adresse in Users-table, wenn dort immer noch keine Adresse
-#  -> Error: Keine Startadresse gegeben.
-def get_address(search_obj):
-    starting_point = search_obj.starting_point
-    if not starting_point:
-        user = SessionLocal.query(schemas.User).filter(schemas.User.id == search_obj.user_id).first()
-        if not user:
-            raise Exception(f"User with id {search_obj.user_id} does not exist")
-        else:
-            if user.address:
-                starting_point = user.address
-            else:
-                raise Exception(f"User with id {search_obj.user_id} does not contain an address.")
 
-    return starting_point
 
 
 #  Google Suche starten und auf Ergebnis warten (mit Anzahl an Vorschlägen)
@@ -105,16 +51,32 @@ def do_search(prompt):
         tools=[{"type": "google_search"}],
         input=prompt,
     )
-    print(interaction, "\n\n")
-    search_results = interaction.output_text
+    unstructured_search_results = interaction.output_text
+    print("Search results (unstructured): \n\n", unstructured_search_results, "\n\n")
     # suggestions = models.SearchCreate.model_validate_json(interaction.output_text)
     # suggestions = client.interactions.create(
     #     model="gemini-3.1-flash-lite",
     #     tools=[{"type": "google_search"}],
     #     input=prompt
     # )
-    print(search_results)
-    return search_results
+    return unstructured_search_results
 
 #  Ergebnis-einholen, Suche abspeichern und Suggestions zum Schluss abspeichern.
-
+def structure_output(unstructured_results: str) -> models.SuggestionList:
+    """
+    Structure the output of unstructured search results.
+    :param unstructured_results: A prior LLM Google-Search result output text.
+    :return suggestions: A models.SuggestionList object.
+    """
+    prompt = "Please extract the relevant information from given input below. \n" + unstructured_results
+    interaction = client.interactions.create(
+        model="gemini-3.1-flash-lite",
+        input=prompt,
+        response_format={
+            "type": "text",
+            "mime_type": "application/json",
+            "schema": models.SuggestionList.model_json_schema()
+        },
+    )
+    suggestions = models.SuggestionList.model_validate_json(interaction.output_text)
+    return suggestions
