0a6018d37fa34cd81f927c48e76cfaf837809abe
Yasin Akkus <akkusoft@gmail.com>
2026-09-15 11:53:24 +0200

    started with just getting the suggestion out of the database over given suggestion_id as a starting point for developing the tripplan creation process.

---
+++ src/pipeline/prepare_trip.py
@@ -0,0 +1,14 @@
+"""
+This module is for the final planning steps getting from a suggestion to a full trip plan.
+"""
+from db.crud import get_suggestion
+from models import TripplanCreate
+
+
+def schedule_flow(user_id: int, suggestion_id: int):
+    """
+    This function is for the final planning steps getting from a suggestion to a full trip plan
+    :return: TripplanCreate object
+    calls get_suggestion() to get suggestion details.
+    """
+    return get_suggestion(user_id=user_id, suggest_id=suggestion_id)
